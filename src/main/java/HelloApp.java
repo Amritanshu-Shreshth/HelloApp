3d15e07af195a0a0c181b2d99b4244d638c76a71
Amritanshu_Shreshth <as5342@srmist.edu.in>
2026-04-03 01:53:52 +0530

    UC5: Use enhanced for loop to display multiple names

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,16 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length > 0) {
+            System.out.print("Hello ");
+
+            // Enhanced for loop
+            for (String name : args) {
+                System.out.print(name + " ");
+            }
+
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
