5f1a602ae70f4e20fe0970efa5bc8b4d2e1eeb14
Amritanshu_Shreshth <as5342@srmist.edu.in>
2026-04-23 02:57:00 +0530

    UC7: Use String.join for multiple names

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,18 +2,8 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            String result = "Hello ";
-
-            // build string with delimiter
-            for (String name : args) {
-                result += name + ", ";
-            }
-
-            // remove last ", " using substring
-            result = result.substring(0, result.length() - 2);
-
-            System.out.println(result);
-
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
         } else {
             System.out.println("Hello, World!");
         }
